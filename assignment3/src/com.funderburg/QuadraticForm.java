6d70ea5ce2af212aab10051e707294604a45c67f
Kevin Funderburg <ksfunderburg@icloud.com>
2018-10-21 18:36:34 -0500

    editing main

--- assignment3/src/com.funderburg/QuadraticForm.java
+++ assignment3/src/com.funderburg/QuadraticForm.java
@@ -1,33 +1,46 @@
 package com.funderburg;
 
+import java.io.*;
 import java.lang.Math;
-
+import java.util.Random;
 
 public class QuadraticForm {
 
     class CalculateQ {
         // Will be solving for formula ax^2 + bx + c = 0
 
-        int a, b, c, x;
-//        double discriminant = (b^2 - 4*a*c);
-        Discriminant d = new Discriminant(a,b,c);
+//        int a, b, c, x;
+//        Discriminant d = new Discriminant(a,b,c);
+//
+//
+//        double p = b + Math.sqrt(d);
+//        double q = b - Math.sqrt(d);
 
 
-        double p = b + Math.sqrt(d);
-        double q = b - Math.sqrt(d);
 
-
-
-        class int Discriminant(a,b,c) {
-            int i = b^2 - 4 * a * c;
-            return i;
-        }
+//        class int Discriminant(a,b,c) {
+//            int i = b^2 - 4 * a * c;
+//            return i;
+//        }
+        // take left side and compare to right side and determine if true or false
     }
 
 
-
-
     public static void main(String [] args) {
+        String currDir = "/Users/kevinfunderburg/Dropbox/Documents/School/2018/Fall/Object Oriented Design/Projects/assignment2.1/src/com/funderburg/",
+                inPath = currDir + "input.txt",
+                outPath = currDir + "output.txt",
+                output = "",
+                str = "";
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(10 + 1);
+        int b = rand.nextInt(10 + 1);
+        int c = rand.nextInt(10 + 1);
+
+
+
 
     }
 }
