e9a95b59ec5aedf09f988b901cd496075b8699a7
Kevin Funderburg <ksfunderburg@icloud.com>
2018-10-20 21:26:16 -0500

    started discriminant class

--- assignment3/src/com.funderburg/QuadraticForm.java
+++ assignment3/src/com.funderburg/QuadraticForm.java
@@ -1,20 +1,30 @@
 package com.funderburg;
 
+import java.lang.Math;
+
+
 public class QuadraticForm {
 
     class CalculateQ {
         // Will be solving for formula ax^2 + bx + c = 0
 
         int a, b, c, x;
-        double root1,root2,
-                discriminant = (b^2 - 4*a*c);
+//        double discriminant = (b^2 - 4*a*c);
+        Discriminant d = new Discriminant(a,b,c);
+
+
+        double p = b + Math.sqrt(d);
+        double q = b - Math.sqrt(d);
 
 
 
-        root1 = b + sqrt(discriminant);
-        root2 = b - sqrt(discriminant);
-
+        class int Discriminant(a,b,c) {
+            int i = b^2 - 4 * a * c;
+            return i;
+        }
     }
+
+
 
 
     public static void main(String [] args) {
--- assignment3/src/quadForm.java
+++ assignment3/src/quadForm.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class quadForm {
+
+
+
+
+    public static void main(String args[]) {
+        System.out.println("hello");
+
+    }
 }
