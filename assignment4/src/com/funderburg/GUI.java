25c389500e668537b3636940b1ba7efb911adc93
Kevin Funderburg <ksfunderburg@icloud.com>
2018-12-01 17:00:55 -0600

    moving on to assignment 5

--- assignment4/src/com/funderburg/GUI.java
+++ assignment4/src/com/funderburg/GUI.java
@@ -1,48 +1,140 @@
 package com.funderburg;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class GUI extends JPanel {
     private boolean DEBUG = false;
+    public static String CALHEADER = "";
+    public static String CALTITLE = "Calendar";
 
     public GUI() {
         super(new GridLayout(1,0));
 
+//        JTextField textField = new JTextField(10);
+//        textField.setActionCommand("someting");
+//        textField.addActionListener(this);
+
         String[] columnNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-        Calendar c = Calendar.getInstance();
-//        c.setTime(yourDate);
+        Calendar cal = Calendar.getInstance();
 
-        int year = 2018, month = 11;
+        int year = 2018, month = 11, day = 19;
 
         int daysInMonth;
 
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||month == 10 || month == 12) {
+        // Get the number of days for the month
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             daysInMonth = 31;
-        } else if (month = 2) {
+        } else if (month == 2) {
             daysInMonth = 28;
         } else {
             daysInMonth = 30;
         }
 
+        // Get the day of the week for the first day of the month
+        String firstDayString = month + "/" + "01" + "/" + year;
+        int firstDayOfMonth = 0;
 
-        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        Date myDate = null;
+        try {
+            myDate = sdf.parse(firstDayString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        sdf.applyPattern("EEE");
+        String sMyDate = sdf.format(myDate);
+        System.out.println(sMyDate);
 
-        Object[][] data = {
-                {"Kathy", "Smith", "Snowboarding", 5, false, "doo", "lee"},
-                {"John", "Doe", "Rowing", 3, true, "doo", "lee"},
-                {"Sue", "Black", "Knitting", 2, false, "doo", "lee"},
-                {"Jane", "White", "Speed reading", 20, true, "doo", "lee"},
-                {"Joe", "Brown", "Pool", 10, false, "doo", "lee"}
-        };
+        switch (sMyDate) {
+            case "Sun":
+                firstDayOfMonth = 0;
+                break;
+            case "Mon":
+                firstDayOfMonth = 1;
+                break;
+            case "Tue":
+                firstDayOfMonth = 2;
+                break;
+            case "Wed":
+                firstDayOfMonth = 3;
+                break;
+            case "Thu":
+                firstDayOfMonth = 4;
+                break;
+            case "Fri":
+                firstDayOfMonth = 5;
+                break;
+            case "Sat":
+                firstDayOfMonth = 6;
+                break;
+        }
+
+        // Get header for calendar
+        switch (month) {
+            case 1:
+                CALHEADER = "January";
+                break;
+            case 2:
+                CALHEADER = "February";
+                break;
+            case 3:
+                CALHEADER = "March";
+                break;
+            case 4:
+                CALHEADER = "April";
+                break;
+            case 5:
+                CALHEADER = "May";
+                break;
+            case 6:
+                CALHEADER = "June";
+                break;
+            case 7:
+                CALHEADER = "July";
+                break;
+            case 8:
+                CALHEADER = "August";
+                break;
+            case 9:
+                CALHEADER = "September";
+                break;
+            case 10:
+                CALHEADER = "October";
+                break;
+            case 11:
+                CALHEADER = "November";
+                break;
+            case 12:
+                CALHEADER = "December";
+                break;
+        }
+
+        // Create calendar data for the table to be shown
+        int x = 1, y = 0;
+        Object[][] data = new Object[6][7];
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (x <= daysInMonth && y >= firstDayOfMonth) {
+                    data[i][j] = x;
+                    x++;
+                } else {
+                    data[i][j] = "";
+                }
+                y++;
+            }
+        }
 
         final JTable table = new JTable(data, columnNames);
         table.setPreferredScrollableViewportSize(new Dimension(500, 250));
@@ -61,6 +153,12 @@ public class GUI extends JPanel {
 
         //Add the scroll pane to this panel.
         add(scrollPane);
+
+        // ASKPROF:  Best method to add a header to the table (refer to https://docs.oracle.com/javase/tutorial/uiswing/components/table.html#show)
+        //           (Have a title with the calendar along a header above the default header of the DOW)
+        // ASKPROF:  Incorporation of hash map to to contain memo data, maybe an array?
+        // ASKPROF:  How to add a text area below or next to the table
+        // ASKPROF:  Use of external calendar libraries: how to do that? Is that better than manually creating?
     }
 
     private void printDebugData(JTable table) {
@@ -86,7 +184,7 @@ public class GUI extends JPanel {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Calendar");
+        JFrame frame = new JFrame(CALTITLE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Create and set up the content pane.
---
+++ assignment5/src/com/funderburg/MyQueue.java
@@ -0,0 +1,171 @@
+/***************************************************************************
+ * Kevin Funderburg
+ * CS 3354 - Programming Assignment 5
+ * Queues
+ *
+ * Contents of MyQueue.java
+ *****************************************************************************/
+
+package com.funderburg;
+
+import java.util.*;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.Random;
+
+//TODO - Decide if this class should be utilized
+public class MyQueue {
+
+    int capacity;
+    int busyTime;
+    ArrayBlockingQueue<Integer> myQueue;
+
+    Random rand = new Random();
+
+    MyQueue(int initialCapacity) {
+        capacity = initialCapacity;
+        busyTime = 0;
+        myQueue = new ArrayBlockingQueue<Integer>(capacity);
+    }
+
+    public void setBusyTime() {
+        // Random time between 1-15 mins that checker is busy
+        busyTime = (rand.nextInt(15) + 1) * 60;
+    }
+
+    //public class MyQueue implements Runnable {
+//
+////    Random rand = new Random();
+////    int busyTime = rand.nextInt(15) + 1;       // Random time between 1-15 mins that checker is busy
+//
+//    @Override
+//    public void run() {
+//
+//        long startTime = System.currentTimeMillis();
+//        System.out.println("[MyQueue] start-time = " + startTime);
+//
+//
+//
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("[MyQueue] end-time = " + endTime);
+//        System.out.println("[MyQueue] time taken = " + (endTime - startTime));
+//
+//
+//    }
+//public class MyQueue extends Thread {
+//
+////    Random rand = new Random();
+////    int busyTime = rand.nextInt(15) + 1;       // Random time between 1-15 mins that checker is busy
+//
+//    @Override
+//    public void run() {
+//
+//        long startTime = System.currentTimeMillis();
+//        System.out.println("[MyQueue] start-time = " + startTime);
+//
+//
+//
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("[MyQueue] end-time = " + endTime);
+//        System.out.println("[MyQueue] time taken = " + (endTime - startTime));
+//
+//
+//    }
+
+
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+        System.out.println("[MyQueue] start-time = " + startTime);
+
+        int POPULATION = 10;                        // Number of people boarding
+
+        int capacityA = (int) (POPULATION * .75);   // queue A capacity
+        int capacityB = POPULATION - capacityA;     // queue B capacity
+        int busyTime = 0;                           // time that the checker is busy
+
+        float times[] = new float[POPULATION];      // array of processing times per person
+        Random rand = new Random();
+
+        ArrayBlockingQueue<Integer> queueA = new ArrayBlockingQueue<Integer>(capacityA);
+        ArrayBlockingQueue<Integer> queueB = new ArrayBlockingQueue<Integer>(capacityB);
+        Queue<Integer> queueC = new LinkedList<>();
+
+//        int theQueue;
+
+        for (int i = 0; i < POPULATION; i++) {
+
+//            theQueue = i % 2;
+//            theQueue = rand.nextInt(1) + 1;
+
+
+            /**
+             * Person is added to queue A or B to be processed
+             */
+
+            // assign a time (in seconds) that a person arrives
+            if (i == 0) {
+                times[i] = 0;
+            } else {
+                times[i] = (rand.nextInt(15) + 1);
+            }
+
+            // Enqueue the person in queue A or B
+            if (queueA.size() < capacityA) {
+                queueA.add(i);
+            } else {
+                queueB.add(i);
+            }
+//            if (theQueue == 0 && queueA.size() < capacityA) {
+//                queueA.add(i);
+//            } else if (theQueue == 1 && queueB.size() < capacityB) {
+//                queueB.add(i);
+//            }
+
+            System.out.println("[MyQueue] person " + i + " arrived at queue [AorB] at " + times[i]);
+
+            // Random time between 1-15 mins that checker is busy
+            busyTime = (rand.nextInt(15) + 1) * 60;
+
+            // Reduce busy time by 1 minute if queues are getting too big
+            if (queueA.size() > 5 || queueB.size() > 5) { busyTime -= 60; }
+
+            // Calculate total processing time for person in first queue
+            times[i] = times[i] + busyTime;
+
+            //TODO - Decide on how to remove from queues A&B (seems like queue should be full before passing to queue C?
+            queueA.remove();
+
+
+            /**
+             * Now the person is added to queue C to be processed
+             */
+
+            queueC.add(i);  // Enqueue
+            System.out.println("[MyQueue] person " + i + " arrived at queue [C] at " + times[i]);
+
+            // Random time between 1-15 mins that checker is busy
+            busyTime = (rand.nextInt(15) + 1) * 60;
+
+            // Reduce busy time by 1 minute if queues are getting too big
+            if (queueC.size() > 5) { busyTime -= 60; }
+
+            times[i] = times[i] + busyTime;     // Calculate total processing time for person
+
+            System.out.println("[MyQueue] person " + i + " finished processing at " + times[i] + "\n");
+
+        }
+
+        float totalProcessTime = 0;
+
+        for (int i = 0; i < times.length; i++)
+            totalProcessTime += times[i];
+
+
+        System.out.println("[MyQueue] total process-time = " + totalProcessTime);
+        long endTime = startTime + (long) (totalProcessTime * 1000);
+        System.out.println("[MyQueue] end-time = " + endTime);
+        System.out.println("[MyQueue] time taken = " + (endTime - startTime));
+    }
+}
+
